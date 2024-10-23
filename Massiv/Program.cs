efd2d1b012b279df42c256727b375e3512e0b9da
Mustafojon <Mustafojon@DESKTOP-P6KNFPB>
2024-10-23 08:43:13 +0300

    Добавьте файлы проекта.

---
+++ Massiv/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace dastur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ismlar = new string[]
+            {
+                "Suhrob",
+                "Muzrob",
+                "Shukrona",
+                "Mustafo"
+            };
+            int[] baholar = new int[]
+            {
+                5,
+                4,
+                4,
+                5
+
+            };
+            int[] random = new int[]
+            {
+                5,
+                4,
+                4,
+                3
+            };
+            Array.Sort(random);
+            Console.WriteLine(random[0]);
+
+            Console.WriteLine($" uzunligi {ismlar.Length} | baholar {baholar.Length}");
+            Console.WriteLine($"1. {ismlar[0]} | {baholar[0]}");
+            Console.WriteLine($"2. {ismlar[1]} | {baholar[1]}");
+
+        }
+    }
+}
